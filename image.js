c2dce88da5ec65f4ede32eb4ea4cac162571584f
arwma-dev <ca5349286@gmail.com>
2025-09-03 02:46:30 +0300

    Add files via upload

---
+++ image.js
@@ -0,0 +1,9 @@
+this.categories = [
+    { id: 1, name: "الوجبات الرئيسية", image: "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80", count: 325 },
+    { id: 2, name: "الحلويات", image: "https://images.unsplash.com/photo-1563729784474-d77dbb933a9e?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80", count: 198 },
+    { id: 3, name: "المقبلات", image: "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80", count: 175 },
+    { id: 4, name: "المشروبات", image: "https://images.unsplash.com/photo-1595857816813-6c39dbfef554?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80", count: 112 },
+    { id: 5, name: "وصفات صحية", image: "https://images.unsplash.com/photo-1494597564530-871f2b93ac55?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80", count: 215 },
+    { id: 6, name: "وصفات سريعة", image: "https://images.unsplash.com/photo-1504674900247-0877df9cc836?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80", count: 187 }
+];
+
